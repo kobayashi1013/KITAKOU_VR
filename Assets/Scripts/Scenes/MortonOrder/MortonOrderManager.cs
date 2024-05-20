73ee3364cf6cbbef9b477f836d61bbf9aa85d1ea
kobayashi <c612101397@tokushima-u.ac.jp>
2024-05-20 16:17:28 +0900

    push

--- Assets/Scripts/Scenes/MortonOrder/MortonOrderManager.cs
+++ Assets/Scripts/Scenes/MortonOrder/MortonOrderManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace Scenes.MortonOrder
 {
@@ -16,13 +17,28 @@ namespace Scenes.MortonOrder
  
         [Header("SceneObjects")]
         [SerializeField] private GameObject _player;
+        [Header("Prefabs")]
+        [SerializeField] private GameObject _avaterPrefab;
         [Header("Parameters")]
         [SerializeField] private MortonOrderDepth _depth = MortonOrderDepth.depth8;
 
         private Vector3 _downPos; //モデルの端
         private Vector3 _upPos;
         private int _prevOrderNumber = -1;
-        private Dictionary<int, List<Vector3>> _otherPosSet = new Dictionary<int, List<Vector3>>(); //Otherの座標集合
+        private Dictionary<int, List<Vector3>> _otherPositionSet = new Dictionary<int, List<Vector3>>(); //Otherの座標集合
+        private ObjectPool<GameObject> _avaterPool;
+
+        void Awake()
+        {
+            //オブジェクトプール設定
+            new ObjectPool<GameObject>(CreatePoolObject, GetPoolObject, ReleasePoolObject, DestroyPoolObject);
+
+            //Other座標集合のキー設定
+            for (int i = 0; i < Mathf.Pow((int)_depth, 3); i++)
+            {
+                _otherPositionSet.Add(i, new List<Vector3>());
+            }
+        }
 
         void Start()
         {
@@ -35,17 +51,9 @@ namespace Scenes.MortonOrder
             foreach (var other in otherList)
             {
                 //Otherオブジェクトの登録
-                var otherMortonPos = ConvertPositionToMorton(other.transform.position);
-                int orderNumber = GetOrderNumber3D((int)otherMortonPos.x, (int)otherMortonPos.y, (int)otherMortonPos.z);
-                if (_otherPosSet.ContainsKey(orderNumber)) //キーが存在する
-                {
-                    _otherPosSet[orderNumber].Add(other.transform.position);
-                }
-                else //キーが存在しない
-                {
-                    _otherPosSet.Add(orderNumber, new List<Vector3>());
-                    _otherPosSet[orderNumber].Add(other.transform.position);
-                }
+                var otherMortonPosition = ConvertToMortonPosition(other.transform.position);
+                int otherOrderNumber = GetOrderNumber3D(otherMortonPosition);
+                _otherPositionSet[otherOrderNumber].Add(other.transform.position);
 
                 //Otherオブジェクトの消去
                 Destroy(other);
@@ -54,40 +62,63 @@ namespace Scenes.MortonOrder
 
         void Update()
         {
-            //座標変換
-            var playerMortonPos = ConvertPositionToMorton(_player.transform.position);
+            //モートンモデル座標に変換
+            var playerMortonPosition = ConvertToMortonPosition(_player.transform.position);
 
             //空間変更の確認
-            int newOrderNumber = GetOrderNumber3D((int)playerMortonPos.x, (int)playerMortonPos.y, (int)playerMortonPos.z);
+            int newOrderNumber = GetOrderNumber3D(playerMortonPosition);
             if (newOrderNumber != _prevOrderNumber)
             {
-                CreateAvater(playerMortonPos);
+                CreateAvater(playerMortonPosition);
                 _prevOrderNumber = newOrderNumber;
             }
         }
 
+        //オブジェクトプール
+        public GameObject CreatePoolObject()
+        {
+            return Instantiate(_avaterPrefab);
+        }
+
+        public void GetPoolObject(GameObject obj)
+        {
+            obj.SetActive(true);
+        }
+
+        public void ReleasePoolObject(GameObject obj)
+        {
+            obj.SetActive(false);
+        }
+
+        public void DestroyPoolObject(GameObject obj)
+        {
+            Destroy(obj);
+        }
+
         //モートン座標への変換
-        private Vector3 ConvertPositionToMorton(Vector3 vec)
+        private Vector3 ConvertToMortonPosition(Vector3 position)
         {
-            var mortonPos = new Vector3(
-                (vec.x - _downPos.x) / transform.localScale.x * (int)_depth,
-                (vec.y - _downPos.y) / transform.localScale.y * (int)_depth,
-                (vec.z - _downPos.z) / transform.localScale.z * (int)_depth);
+            var mortonPosition = new Vector3(
+                (position.x - _downPos.x) / transform.localScale.x * (int)_depth,
+                (position.y - _downPos.y) / transform.localScale.y * (int)_depth,
+                (position.z - _downPos.z) / transform.localScale.z * (int)_depth);
 
-            return mortonPos;
+            return mortonPosition;
         }
 
-        private int GetOrderNumber3D(int x, int y, int z)
+        //モートン空間番号を取得
+        private int GetOrderNumber3D(Vector3 position)
         {
-            if (x < 0 || x >= (int)_depth
-                || y < 0 || y >= (int)_depth
-                || z < 0 || z >= (int)_depth) return -1;
+            if (position.x < 0 || position.x >= (int)_depth
+                || position.y < 0 || position.y >= (int)_depth
+                || position.z < 0 || position.z >= (int)_depth) return -1;
 
-            return BitSeparate3D(x)
-                | BitSeparate3D(y) << 1
-                | BitSeparate3D(z) << 2;
+            return BitSeparate3D((int)position.x)
+                | BitSeparate3D((int)position.y) << 1
+                | BitSeparate3D((int)position.z) << 2;
         }
 
+        //ビット分割（3D)
         private int BitSeparate3D(int data)
         {
             data = (data | data << 8) & 0x0000f00f;
@@ -96,17 +127,33 @@ namespace Scenes.MortonOrder
             return data;
         }
 
-        private void CreateAvater(Vector3 playerMortonPos)
+        //アバター生成
+        private void CreateAvater(Vector3 position)
         {
+            //前後空間
             int[] dx = { -1, 0, 1, -1, 0, 1, -1, 0, 1, -1, 0, 1, -1, 0, 1, -1, 0, 1, -1, 0, 1, -1, 0, 1, -1, 0, 1 };
             int[] dy = { -1, -1, -1, 0, 0, 0, 1, 1, 1, -1, -1, -1, 0, 0, 0, 1, 1, 1, -1, -1, -1, 0, 0, 0, 1, 1, 1 };
             int[] dz = { -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
+            //モートン空間番号を取得
             var orderNumbers = new List<int>();
             for (int i = 0; i < 27; i++)
             {
-                orderNumbers.Add(GetOrderNumber3D((int)playerMortonPos.x + dx[i], (int)playerMortonPos.y + dy[i], (int)playerMortonPos.z + dz[i]));
-                Debug.Log(orderNumbers[i]);
+                int orderNumber = GetOrderNumber3D(new Vector3(position.x + dx[i], position.y + dy[i], position.z + dz[i]));
+                orderNumbers.Add(orderNumber);
+            }
+
+            //モートン空間外を除外
+            orderNumbers.RemoveAll(x => x.Equals(-1));
+
+            //アバターを配置
+            foreach (var orderNumber in orderNumbers)
+            {
+                foreach (var otherPosition in _otherPositionSet[orderNumber])
+                {
+                    var avater = _avaterPool.Get();
+                    avater.transform.position = otherPosition;
+                }
             }
         }
     }
