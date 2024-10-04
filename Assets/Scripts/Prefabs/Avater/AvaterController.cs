d17a6b4be64be35ecfa322d99e124868a8af3247
kohasuke <110949581+kobayashi1013@users.noreply.github.com>
2024-10-04 17:08:05 +0900

    アバターアクションの実装

--- Assets/Scripts/Prefabs/Avater/AvaterController.cs
+++ Assets/Scripts/Prefabs/Avater/AvaterController.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
@@ -9,37 +8,84 @@ namespace Prefabs.Avater
 {
     public class AvaterController : MonoBehaviour
     {
-        [Header("回転")]
+        [Header("時間")]
         [SerializeField] private float _rotationTime = 0f;
+        [SerializeField] private float _moveTime = 0f;
         [SerializeField] private float _minWaitingTime = 0f;
         [SerializeField] private float _maxWaitingTime = 0f;
 
+        private Rigidbody _rigidbody;
+
         private void Start()
         {
-            Action(); //アクション開始
+            //コンポーネント取得
+            _rigidbody = GetComponent<Rigidbody>();
+
+            //アクション開始
+            Action();
         }
 
         private async void Action()
         {
             while (true)
             {
-                await Rotation();
+                float probability = UnityEngine.Random.Range(0f, 1f);
+                if (probability < 0.5) await Rotation();
+                else await Move();
             }
         }
 
+        /// <summary>
+        /// アバターの回転
+        /// </summary>
+        /// <returns></returns>
         private async UniTask Rotation()
         {
             //回転角度の決定
             Quaternion currentRotation = transform.rotation;
             Quaternion randomRotation = Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 360f), 0f);
 
-            //プレイヤーの回転
+            //回転
             float timer = 0f;
             while (timer < _rotationTime)
             {
                 if (this == null) break; //エラーハンドリング
                 timer += Time.deltaTime;
                 transform.rotation = Quaternion.Lerp(currentRotation, randomRotation, timer / _rotationTime);
+                await UniTask.Yield(PlayerLoopTiming.Update);
+            }
+
+            //ランダム時間待機
+            float waitingTime = UnityEngine.Random.Range(_minWaitingTime, _maxWaitingTime);
+            await UniTask.Delay(TimeSpan.FromSeconds(waitingTime));
+        }
+
+        private async UniTask Move()
+        {
+            //回転角度の決定
+            Quaternion currentRotation = transform.rotation;
+            Quaternion randomRotation = Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 360f), 0f);
+
+            //回転
+            float timer = 0f;
+            while (timer < _rotationTime)
+            {
+                if (this == null) break; //エラーハンドリング
+                timer += Time.deltaTime;
+                transform.rotation = Quaternion.Lerp(currentRotation, randomRotation, timer / _rotationTime);
+                await UniTask.Yield(PlayerLoopTiming.Update);
+            }
+
+            //移動位置の決定
+            Vector3 movement = randomRotation * Vector3.forward;
+
+            //移動
+            timer = 0f;
+            while (timer < _rotationTime)
+            {
+                if (this == null) break; //エラーハンドリング
+                timer += Time.deltaTime;
+                _rigidbody.MovePosition(_rigidbody.position + movement * Time.deltaTime);
                 await UniTask.Yield(PlayerLoopTiming.Update);
             }
 
